36d413ab647eedb301896848bc4ab9ac866e73aa
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-12 23:22:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
-
 var array = new Array()
 function forLoop(array){
 
   for (let i = 0; i <= 25; i++) {
     array.push(`"I am ${i} strange loop${i===1 ? '' : 's'}."`)
   }
+  
+  return array
 }
 
 function whileLoop(i){
