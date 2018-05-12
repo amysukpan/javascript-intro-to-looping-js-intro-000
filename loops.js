268a8f330ba2bee854362034b663b7c1a5eb1db4
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-12 23:22:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,25 @@
+
+var array = new Array()
+function forLoop(array){
+
+  for (let i = 0; i <= 25; i++) {
+    array.push(`"I am ${i} strange loop${i===1 ? '' : 's'}."`)
+  }
+}
+
+function whileLoop(i){
+  while (i>0){
+    console.log(i--)
+  }
+  return "done"
+}
+
+function doWhileLoop(array){
+  do {
+      array.pop()
+  } while (array.length!=0 || maybeTrue());
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
